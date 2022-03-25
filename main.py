667cc6dd004f2520aa6efe757062f26cb66306cb
JosLeDeta <josele.96@gmail.com>
2022-03-25 17:54:33 +0100

    feat: scrap multiple pages

--- main.py
+++ main.py
@@ -2,14 +2,17 @@ from bing_utils import SimpleSearch
 from multiprocessing import Process
 import json
 
-terms = ['test', 'scrapping', 'test typefile: pdf inurl: *.pdf']
+terms = ['scrapping', 'test typefile: pdf inurl: *.pdf', 'enthec']
 
 def ThreadSearch(term, n_pages=1):
-    data = SimpleSearch(term, n_page=1)
+    print(f'[i] Scrapping term {term} with total pages: {n_pages}')
+    data = []
+    for n in range(n_pages):
+        data.extend(SimpleSearch(term, n_page=n + 1))
     name = term.replace(' ', '-').replace(':', '').replace('*', '').replace('.','')
     with open(f'json_files/{name}.json', 'w', encoding='utf-8') as file:
         file.write(json.dumps(data, ensure_ascii=False))
 
 if __name__ == '__main__':
     for term in terms:
-        Process(target=ThreadSearch, args=[term, ]).start()
+        Process(target=ThreadSearch, args=[term, 5]).start()

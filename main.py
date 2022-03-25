3cb9cb69c694cb08da4918d8aa84611273ec3168
JosLeDeta <josele.96@gmail.com>
2022-03-25 17:16:27 +0100

    feat: add threadsearch and json save

--- main.py
+++ main.py
@@ -1,4 +1,16 @@
 from bing_utils import SimpleSearch, BingResult
+from multiprocessing import Process
+import json
 
+terms = ['test', 'scrapping', 'test typefile: pdf inurl: *.pdf']
 
-print(SimpleSearch('test', 1))
+def ThreadSearch(term, n_pages=1):
+    data = SimpleSearch(term, n_page=1)
+    name = term.replace(' ', '-').replace(':', '').replace('*', '').replace('.','')
+    with open(f'json_files/{name}.json', 'w', encoding='utf-8') as file:
+        file.write(json.dumps(data, ensure_ascii=False))
+
+if __name__ == '__main__':
+    n_tasks = len(terms)
+    for term in terms:
+        Process(target=ThreadSearch, args=[term, ]).start()
